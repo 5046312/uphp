a7b57f3010eb5ea2d3a3096438b891be20bdbcfa
PC-20161130TGIX\Administrator <5046312@qq.com>
2017-09-01 10:23:16 +0800

    独行单行注释改用#

--- uphp/index.php
+++ uphp/index.php
@@ -11,20 +11,20 @@ $GLOBALS['uphp']['urlInfo']['m'] = $_GET['m'] ?: START_MODULE;
 $GLOBALS['uphp']['urlInfo']['c'] =  $_GET['c'] ?: START_CONTROLLER;
 $GLOBALS['uphp']['urlInfo']['a'] =  $_GET['a'] ?: START_ACTION;
 
-// 加载数据库设置
+# 加载数据库设置
 $GLOBALS['uphp']['config'] = include(CONFIG_DIR.'/config.php');
 
-// 引入Db基类
+# 引入Db基类
 include(U_DIR.'/lib/Mysql.php');
-// 引入Model基类
+# 引入Model基类
 include(U_DIR.'/lib/Model.php');
 # 引入模板引擎
 include_once(U_DIR."/lib/View.php");
-// 引入控制器基类
+# 引入控制器基类
 include(U_DIR.'/lib/Controller.php');
-// 引用控制器
+# 引用控制器
 include(APP_DIR.'/'.$GLOBALS['uphp']['urlInfo']['m'].'/controller/'.$GLOBALS['uphp']['urlInfo']['c'].'Controller'.'.php');
-// 带命名空间类的实例化
+# 带命名空间类的实例化
 $class = APP_DIR.'\\'.$GLOBALS['uphp']['urlInfo']['m'].'\controller\\'.$GLOBALS['uphp']['urlInfo']['c'].'Controller';
 $controller = new $class();
 $action = $controller->$GLOBALS['uphp']['urlInfo']['a']();
