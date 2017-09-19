8fe1bd9f7fa7fca556933a37e227d9fb9e63e80b
USER-20160718IS\5046312 <5046312@qq.com>
2017-09-19 19:01:27 +0800

    精简了Controller基类

--- Uphp/Library/Controller.php
+++ Uphp/Library/Controller.php
@@ -8,68 +8,17 @@ namespace Uphp;
  */
 class Controller
 {
-    public $viewVariable = []; // 模板变量
-    public $logType; // 日志类型
-
     public function __construct()
     {
 
     }
 
-    /**
-     * $this->redirect("indexController/login", ["a" => "123", "b" => "456"])
-     * @param $url
-     * @param $param
-     * @param $refresh
-     * @param $view
-     */
     public function redirect($url, $param, $refresh, $view){
         jump(url($url, $param), $refresh, $view);
     }
 
-    /**
-     * 绑定模板变量
-     * @param $key
-     * @param $value
-     */
-    public function fetch($key, $value){
-        if(is_array($key)){
-            foreach($key as $k=>$v){
-                $this->viewVariable[$k] = $v;
-            }
-        }else{
-            $this->viewVariable[$key] = $value;
-        }
-    }
+    public function view($tpl){
 
-    /**
-     * 输出模板
-     * @param $tpl 指定输出模板(仅限当前模块)
-     * @param array $data 指定变量赋值
-     */
-    public function view($tpl, $data){
-        # 附带变量渲染模板
-        if(!empty($data)){
-            foreach($data as $k=>$v){
-                $this->viewVariable[$k] = $v;
-            }
-        }
-        # 指定输出模板
-        if(!empty($tpl)){
-            # $tpl "indexController" or "hello/asd"
-            if(strpos($tpl, "/")){
-                $tpl = explode("/", $tpl);
-                $tplFile = "./".APP_DIR."/".$GLOBALS['Uphp']['urlInfo']['m']."/View/".$tpl[0]."/".$tpl[1].Config::get("view_suffix");
-            }else{
-                $tplFile = "./".APP_DIR."/".$GLOBALS['Uphp']['urlInfo']['m']."/View/".$GLOBALS['Uphp']['urlInfo']['c']."/".$tpl.Config::get("view_suffix");
-            }
-        }else{
-            # empty to find same action template
-            $tplFile = "./".APP_DIR."/".$GLOBALS['Uphp']['urlInfo']['m']."/View/".$GLOBALS['Uphp']['urlInfo']['c']."/".$GLOBALS['Uphp']['urlInfo']['a'].Config::get("view_suffix");
-        }
-
-        $view = new View($tplFile, $this->viewVariable);
-        return $view->show();
     }
 
     /**
@@ -87,9 +36,5 @@ class Controller
                 header("Content-type:text/xml; charset=utf-8");
                 die(xmlEncode($data));
         }
-    }
-
-    public function __call($method, $arguments){
-        Error::exception(Language::get("ACTION_NOT_EXIST").":".$method);
     }
 }
