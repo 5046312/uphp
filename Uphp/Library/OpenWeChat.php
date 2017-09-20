83985447af05754cabff21b521fe90e56d9a20fe
USER-20160718IS\5046312 <5046312@qq.com>
2017-09-20 22:55:46 +0800

    OpenWeChat重新独立出来，全部静态化，功能初步定义完成，准备填充。

--- Uphp/Library/OpenWeChat.php
+++ Uphp/Library/OpenWeChat.php
@@ -1,33 +1,33 @@
 <?php
 namespace Uphp;
 
-class OpenWeChat extends Controller
+class OpenWeChat
 {
     /**
      * 获取微信配置信息
      * @var
      */
-    private $OpenWeChatConfig;
+    private static $OpenWeChatConfig;
 
-    public function __construct()
+    private static function init()
     {
-        parent::__construct();
-        $this->OpenWeChatConfig = config('OpenWeChat');
+        self::$OpenWeChatConfig = config('OpenWeChat');
         #   获取access_token，使用配置中缓存方式储存
-
-        p($this->getAccessToken());
-
     }
 
-    private function getAccessToken(){
+    /**
+     * 获取access_token
+     * @return mixed
+     */
+    protected static function getAccessToken(){
         if($access_token = Cache::get("ACCESS_TOKEN")){
             return $access_token;
         }else{
-            $url = "https://{$this->OpenWeChatConfig['WeChatDomain']}/cgi-bin/token?grant_type=client_credential&appid={$this->OpenWeChatConfig['appId']}&secret={$this->OpenWeChatConfig['appSecret']}";
+            $url = "https://".self::$OpenWeChatConfig['WeChatDomain']."/cgi-bin/token?grant_type=client_credential&appid=".self::$OpenWeChatConfig['appId']."&secret=".self::$OpenWeChatConfig['appSecret'];
             #   尝试次数
-            for ($i=0; $i<$this->OpenWeChatConfig['timeout']; $i++){
+            for ($i=0; $i<self::$OpenWeChatConfig['timeout']; $i++){
                 $r = json_decode(curlGet($url), true);
-                if(!isset($r['errcode'])){
+                if(!isset($r['errcode']) || $r['errcode'] == 0){
                     #   正确获取，缓存预留十分钟
                     Cache::set("ACCESS_TOKEN", $r['access_token'], $r['expires_in'] - 600);
                     Log::add("OpenWeChat # 获取access_token");
@@ -39,5 +39,108 @@ class OpenWeChat extends Controller
         }
     }
 
+    /**
+     * 自定义菜单
+     */
+    public static function menu(){
 
+    }
+
+    /**
+     * 消息管理
+     */
+    public static function message(){
+
+    }
+
+    /**
+     * 素材管理
+     */
+    public static function material(){
+
+    }
+
+    /**
+     * 用户管理
+     */
+    public static function user(){
+
+    }
+
+    /**
+     * 账号管理
+     */
+    public static function account(){
+
+    }
+
+    /**
+     * 数据统计
+     */
+    public static function dataCube(){
+
+    }
+
+    /**
+     * 微信卡卷
+     */
+    public static function card(){
+
+    }
+
+    /**
+     * 微信门店
+     */
+    public static function business(){
+
+    }
+
+    /**
+     * 微信小店
+     */
+    public static function shop(){
+
+    }
+
+    /**
+     * 微信设备功能
+     */
+    public static function device(){
+
+    }
+
+    /**
+     * 客服功能
+     */
+    public static function customService(){
+
+    }
+
+    /**
+     * 摇一摇
+     */
+    public static function shake(){
+
+    }
+
+    /**
+     * 微信连wifi
+     */
+    public static function wifi(){
+
+    }
+
+    /**
+     * 扫一扫
+     */
+    public static function scan(){
+
+    }
+
+    /**
+     * 微信发票
+     */
+    public static function invoice(){
+
+    }
 }
--- app/Controller/index/indexController.php
+++ app/Controller/index/indexController.php
@@ -3,9 +3,8 @@ namespace app\Controller\index;
 use app\Model\UserModel;
 use Uphp\Cache;
 use Uphp\Controller;
-use Uphp\OpenWeChat;
 
-class indexController extends OpenWeChat
+class indexController extends Controller
 {
     public function index(){
 
