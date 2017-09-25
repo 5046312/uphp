7a4586d81f3b339ab77e295da31f191cee3bb177
PC-20161130TGIX\Administrator <5046312@qq.com>
2017-09-25 17:05:30 +0800

    OpenWeChat封装的User进行函数命名

--- Uphp/Library/Driver/OpenWeChat/User.php
+++ Uphp/Library/Driver/OpenWeChat/User.php
@@ -1,8 +1,121 @@
 <?php
 namespace Uphp\Driver\OpenWeChat;
 
-
-class User
+/**
+ * 用户管理
+ * Class User
+ * @package Uphp\Driver\OpenWeChat
+ */
+class User extends OpenWeChat
 {
+    /**
+     * 创建标签
+     */
+    protected function tagsCreate(){
 
+    }
+
+    /**
+     * 获取已创建的标签
+     */
+    protected function tagsGet(){
+
+    }
+
+    /**
+     * 编辑标签
+     */
+    protected function tagsUpdate(){
+
+    }
+
+    /**
+     * 删除标签
+     */
+    protected function tagsDelete(){
+
+    }
+
+    /**
+     * 获取标签下用户
+     */
+    protected function tagsUser(){
+
+    }
+
+    /**
+     * 为用户打标签
+     */
+    protected function userAddTags(){
+
+    }
+
+    /**
+     * 为用户取消标签
+     */
+    protected function userDelTags(){
+
+    }
+
+    /**
+     * 获取用户身上的标签列表
+     */
+    protected function getUserTags(){
+
+    }
+
+    /**
+     * 设置用户备注名
+     */
+    protected function setMark(){
+
+    }
+
+    #   获取用户基本信息
+    /**
+     * 获取用户基本信息
+     */
+    protected function getInfo(){
+
+    }
+
+    /**
+     * 批量获取用户基本信息
+     */
+    protected function getInfos(){
+
+    }
+
+    #   获取用户列表
+
+    /**
+     * 获取用户列表
+     */
+    protected function get(){
+
+    }
+
+    #   获取用户地理位置（每次进入公众号则push一个Event、Location）
+
+    #   黑名单管理
+    /**
+     * 获取黑名单列表
+     */
+    protected function blackList(){
+
+    }
+
+    /**
+     * 拉黑
+     */
+    protected function setBlack(){
+
+    }
+
+    /**
+     * 取消拉黑
+     */
+    protected function unsetBlack(){
+
+    }
 }
